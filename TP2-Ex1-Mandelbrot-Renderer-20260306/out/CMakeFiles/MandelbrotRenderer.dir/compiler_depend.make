f1cc0a5325d8461979374add0487d5478c20d6df
fromScratch-ToDev <thibault.malespine@gmail.com>
2026-03-09 09:32:06 +0100

    delete old folders

--- TP2-Ex1-Mandelbrot-Renderer-20260306/out/CMakeFiles/MandelbrotRenderer.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for MandelbrotRenderer.
-# This may be replaced when dependencies are built.
--- TP2-Ex1-Mandelbrot-Renderer-20260306/out/extern/SDL2/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/thibaultmalespine/GPGPU@d754ca3b3cda7ac63f1d5ef32953635dc5086679"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
