c3eeda07c5fba5ee21f82bcab15c747b818947f6
fromScratch-ToDev <thibault.malespine@gmail.com>
2026-03-20 13:26:47 +0100

    fin exo1 TP3

--- TP3/gpu.cu
+++ TP3/gpu.cu
@@ -7,8 +7,8 @@
 __global__ void kernel(int *data, int *count, uint32_t sampleNb, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (idx < sampleNb) {
-        for (int i = (idx - 1) * N; i < idx * N; i++) {
+    if (idx * N < sampleNb) {
+        for (int i = idx * N; i < (idx + 1) * N; i++) {
             int value = data[i];
             atomicAdd(&count[value], 1);
         }
@@ -16,25 +16,28 @@ __global__ void kernel(int *data, int *count, uint32_t sampleNb, int N) {
 }
 
 void gpu_histogramme(int* data, int* count, uint32_t sampleNb, uint32_t distributionSize, int N) {
-    ChronoGPU chrGPU;
-    chrGPU.start();
-
+    
     // allocation mémoire
     int *d_data, *d_count;
     cudaMalloc(&d_data, sampleNb * sizeof(int));
     cudaMalloc(&d_count, distributionSize * sizeof(int));
-
+    
     // copie des données
     cudaMemcpy(d_data, data, sampleNb * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemset(d_count, 0, distributionSize * sizeof(int));
     
     // lancement du kernel
-    kernel<<<((distributionSize/512) + 1) / N, 512>>>(d_data, d_count, sampleNb, N);
+    ChronoGPU chrGPU;
+    chrGPU.start();
 
-    // copie du résultat
-    cudaMemcpy(count, d_count, distributionSize * sizeof(int), cudaMemcpyDeviceToHost);
+    int blocSize = 512;
+    kernel<<<(((sampleNb/blocSize) + 1) / N)+1, blocSize>>>(d_data, d_count, sampleNb, N);
 
     chrGPU.stop();
     printf("Temps GPU: %f ms\n", chrGPU.elapsedTime());
+    
+    // copie du résultat
+    cudaMemcpy(count, d_count, distributionSize * sizeof(int), cudaMemcpyDeviceToHost);
     
     // nettoyage
     cudaFree(d_data);
